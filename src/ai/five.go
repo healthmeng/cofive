452ac60d4009d3c057bbca0507c343da211c0e16
healthmeng <24625527@qq.com>
2017-04-27 14:58:16 +0800

    update ai

--- src/ai/five.go
+++ src/ai/five.go
@@ -78,18 +78,61 @@ func (player* AIPlayer)Evaluate()(int,int){
 	return 0,0
 }
 
+func (player* AIPlayer)chessaround(x,y int) bool{
+	for i:=x-2;i<=x+2,i++{
+		if x<0 || x>15{
+			continue
+		}
+		for j:=y-2;j<y+2;j++{
+			if y<0 || y>15{
+				continue
+			}
+			if player.frame[i][j]{
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (player* AIPlayer)getallstep()[]StepInfo{
+	var sts:=make ([]StepInfo,0,MAX_STEP)
+	for i:=0;i<15;i++{
+		for j:=0;j<15;j++{
+			if player.frame[i][j]==0 && chessaound(i,j) {
+				sts=append(std,StepInfo{i,j,0})
+			}
+		}
+	}
+	return nil
+}
+
 func (player* AIPlayer)DirectAlgo()StepInfo{
 	var st StepInfo
 	allst:=player.getallstep()
 	nstep:=len(allst)
-	if(nstep<1)
-		return nil
+//	if(nstep<1)
+//		return nil
 	results:=make ([]StepInfo,0,nstep)
+	maxscore:=SCORE_INIT
 	for i:=0;i<nstep;i++{
 		player.ApplyStep(allst[i])
 		bscore,wscore:=player.Evaluate()
+		scores:=[2]int{bscore,wscore}
+		val:=scores[player.computer]-scores[player.human]
+		if val>maxscore{
+			results=make([]stepInfo,1,nstep)
+			results[0]=allst[i]
+			maxscore=val
+		}else if val==maxscore{
+			results=append(results,allst[i])
+		}
 	}
-	return st
+	nchoose:=len(results)
+	if nchoose>0{
+		return results[rnd.Int()%nchoose]
+	}
+	return nil
 }
 
 func (player* AIPlayer)MinMaxAlgo()StepInfo{
