73239a4284a7581f8a6576bc8904e9ef14f72a2e
healthmeng <24625527@qq.com>
2020-01-15 14:25:40 +0800

    web processing on going

--- src/cofived/main.go
+++ src/cofived/main.go
@@ -9,6 +9,11 @@ import (
 "bufio"
 "net"
 "sync"
+"net/http"
+"html/template"
+"strconv"
+"strings"
+"encoding/json"
 )
 
 /*
@@ -25,6 +30,15 @@ type StepsInfo struct{
 	level int
 }
 
+type CurStepInfo struct{
+	BVal int
+	WVal int
+	X	int
+	Y	int
+	IsOver int
+	Id	string
+}
+
 func CreateRand() int64{
 	rand.Seed(time.Now().UnixNano())
 	return rand.Int63()
@@ -34,7 +48,13 @@ type CalcData struct{
 	player *ai.AIPlayer
 	chRet chan int
 }
-
+/*
+type GameInfo struct{
+	Level string 
+	Color string
+	Forbid string
+}
+*/
 var tkdata map[int64] *CalcData
 var maplock sync.RWMutex
 
@@ -63,14 +83,14 @@ func CreateBySteps(info *StepsInfo)(*ai.AIPlayer,int64){
 	}
 	return p,id
 }
-func ProcCurrent(conn net.Conn,p* ai.AIPlayer, id int64){
-		bval,wval:=p.GetCurValues()
+func ProcCurrent(p* ai.AIPlayer, id int64){
+/*		bval,wval:=p.GetCurValues()
 		over:=p.IsOver()
 		res:=fmt.Sprintf("%d %d %d %d\n",over,id,bval,wval)
 		conn.Write([]byte(res))
 		if over!=0{
 			return
-		}
+		}*/
 		cdata:=CalcData{p,make (chan int)}
 		maplock.Lock()
 		tkdata[id]=&cdata
@@ -82,11 +102,11 @@ func ProcCurrent(conn net.Conn,p* ai.AIPlayer, id int64){
 				maplock.Unlock()
 			}()
 			x,y:=player.GetStep(false)
-			over=player.IsOver()
-			bval,wval=player.GetCurValues()
+			over:=player.IsOver()
+			bval,wval:=player.GetCurValues()
 			cdata.chRet<-x
 			select {
-			case <-time.After(time.Second*60):
+			case <-time.After(time.Second*30):
 			case cdata.chRet<-y:
 				cdata.chRet<-over
 				cdata.chRet<-bval
@@ -137,13 +157,76 @@ func procConn(conn net.Conn){
 			fmt.Sscanf(string(line),"%d%d",&steps.x[i],&steps.y[i])
 		}
 		p,id:=CreateBySteps(&steps)
-		ProcCurrent(conn,p,id)
+		bval,wval:=p.GetCurValues()
+		over:=p.IsOver()
+		res:=fmt.Sprintf("%d %d %d %d\n",over,id,bval,wval)
+		conn.Write([]byte(res))
+		if over!=0{
+			return
+		}
+		ProcCurrent(p,id)
 	case "GetFromID":
 		line,_,err=rd.ReadLine()
 		var id int64
 		fmt.Sscanf(string(line),"%d",&id)
 		ProcNext(conn,id)
 	}
+}
+
+func StartGame(w http.ResponseWriter, r *http.Request){
+   // w.Header().Set("Access-Control-Allow-Origin", "*")             //允许访问所有域
+    //w.Header().Add("Access-Control-Allow-Headers", "Content-Type") 
+	if r.Method=="GET"{
+		r.ParseForm()
+		t,_:=template.ParseFiles("front.htm")
+		data:=make(map[string]string)
+		data["Level"]=r.Form.Get("level")
+		data["Color"]=r.Form.Get("color")
+		data["Forbid"]=r.Form.Get("forbid")
+		t.Execute(w,data)
+	}
+}
+
+func HttpPostStep(w http.ResponseWriter, r *http.Request){
+    //w.Header().Set("Access-Control-Allow-Origin", "*")             //允许访问所有域
+    //w.Header().Add("Access-Control-Allow-Headers", "Content-Type")
+	if r.Method=="POST"{
+		if err:=r.ParseForm();err!=nil{
+			fmt.Println("parse form error:",err)
+		}else{
+			fmt.Printf("level:%s, forbid %s, num:%s", r.FormValue("level"),r.FormValue("forbid"),r.FormValue("num"))
+		}
+		var steps StepsInfo
+		steps.level,_=strconv.Atoi(r.FormValue("level"))
+		if r.FormValue("forbid")=="1"{
+			steps.forbid=true
+		}
+		num,_:=strconv.Atoi(r.FormValue("num"))
+		steps.x=make([]int,num)
+		steps.y=make([]int,num)
+		points:=r.FormValue("steps") // a,b;c,d;e,f
+		xyinfo:=strings.Split(points,";")
+		for i:=0;i<num;i++{
+			pt:=strings.Split(xyinfo[i],",")
+			steps.x[i],_=strconv.Atoi(pt[0])
+			steps.y[i],_=strconv.Atoi(pt[1])
+		}
+		p,id:=CreateBySteps(&steps)
+		var curinfo CurStepInfo
+		curinfo.BVal,curinfo.WVal=p.GetCurValues()
+		curinfo.X,curinfo.Y=p.GetLastStep()
+		curinfo.IsOver=p.IsOver()
+		curinfo.Id=fmt.Sprintf("%d",id)
+		if obj,err:=json.Marshal(&curinfo);err!=nil{
+			fmt.Println("Marshal error")
+			return
+		}else{
+			fmt.Fprint(w,string(obj))
+		}
+	}
+}
+
+func HttpGetID(w http.ResponseWriter, r *http.Request){
 }
 
 func main(){
@@ -154,12 +237,22 @@ func main(){
 		return
 	}
 	defer lis.Close()
-	for{
-		if conn,err:=lis.Accept();err!=nil{
-			fmt.Println("Accept error:",err)
-		}else{
-			go procConn(conn)
+	go func (){
+		for{
+			if conn,err:=lis.Accept();err!=nil{
+				fmt.Println("Accept error:",err)
+			}else{
+				go procConn(conn)
+			}
 		}
+	}()
+
+	http.HandleFunc("/",StartGame)
+	http.HandleFunc("/PostStep",HttpPostStep)
+	http.HandleFunc("/GetFromID",HttpGetID)
+	err=http.ListenAndServe(":7777",nil)
+	if err!=nil{
+		fmt.Println("error:",err)
 	}
 }
 
